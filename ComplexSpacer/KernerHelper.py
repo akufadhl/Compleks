9bd69324d078a56019bf9ffc53d2fc184043714a
Fadhl Haqq <punyafadhl@gmail.com>
2022-10-27 16:38:25 +0800

    delete old and renew classes

--- ComplexSpacer/KernerHelper.py
+++ ComplexSpacer/KernerHelper.py
@@ -1,33 +1,30 @@
-class valueRecord(object):
-    
-    def __init__(self, xPlace :int, xAdv :int, yPlace :int = 0, yAdv :int = 0):
-        self.xPlacement = xPlace
-        self.yPlacement = yPlace
-        self.xAdvance = xAdv
-        self.yAdvance = yAdv
+class anchor(object):
+
+    def __init__(self, name: str):
+        pass
+        
+class glyphObject(object):
+
+    def __init__(self, width: int, height: int, category: str, script: str, anchors: anchor):
+        self.width = width
+        self.height = height
+        self.category = category
+        self.script = script
+        self.anchors = anchors
     
     def __str__(self):
-        return f"<{self.xPlacement} {self.yPlacement} {self.xAdvance} {self.yAdvance}>"
+        pass
 
-class kernObject(object):
+class kerningPairs(object):
 
-    def __init__(self):
-        self.affecting = list()
-        self.affected = list()
-        self.affector = list()
-        self.kern = valueRecord
-        self.script = str()
+    def __init__(self, value: int, Affected: list, Affecting: list, Affector: list = None):
+        self.value = value
+        self.affected = Affected
+        self.affecting = Affecting
+        self.affector = Affector
+    
+    def isContextual():
+        return self.affector != None
 
-    def __str__(self):
-        affecting = " ".join(str(x) for x in self.affecting)
-        affected = " ".join(str(x) for x in self.affected)
-        affector = " ".join(str(x) for x in self.affector)
-        return f"pos [{affecting}] [{affected}]' [{affector}] {self.kern};"
-
-kern1 = kernObject()
-kern1.affecting = ['o-java', 'suku-java']
-kern1.affected = ['na-java', 'ma-java']
-kern1.affector = ['naMurdaPas-java']
-kern1.kernValue = valueRecord(250,250,250,250)
-
-print(kern1)
+    def kern():
+        pass
